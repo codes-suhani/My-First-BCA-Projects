f2e7644d2e510ccd270a5d7169c4a6d184a3f1ac
codes-suhani <suhani11@amityonline.com>
2026-09-24 19:41:45 +0530

    Create even_odd.py ( Add Python script to check even or odd number)
    Created a simple python program that checks whether a given number is even or odd, along with clean formatting and correct syntax

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+print("--- Welcome to My First Python Project ---\n")
+number = 4
+if number % 2 == 0:
+    print(f"The number {number} is EVEN!")
+else:
+    print(f"The number {number} is ODD!")
+    print("Thanks for checking out my code!")
+  
